c36170e9640d0572c6d615b3e7b1d25a311cdd41
Aliki Yiannakou <alikiyiannakou@gmail.com>
2015-03-09 10:14:40 +0000

    test for user class

---
+++ test/com/twu/biblioteca/ItemOfBibliotecaTest.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by ayiannak on 09/03/2015.
+ */
+public class ItemOfBibliotecaTest {
+}
---
+++ test/com/twu/biblioteca/UserTest.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+import com.twu.entities.User;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+/**
+ * Created by ayiannak on 09/03/2015.
+ */
+public class UserTest {
+    User userTest = new User("Aliki", "passwordTest", "444-4444", "aliki@gmail.com", "7478292");
+
+    StringBuilder testResult = new StringBuilder();
+    String newLine = System.getProperty("line.separator");
+
+    @Test
+    public void testToString() {
+        testResult.append("Name: ");
+        testResult.append("Aliki");
+        testResult.append(newLine);
+        testResult.append("Email: ");
+        testResult.append("aliki@gmail.com");
+        testResult.append(newLine);
+        testResult.append("Phone number: ");
+        testResult.append("7478292");
+        testResult.append(newLine);
+
+        assertEquals(testResult.toString(),userTest.toString());
+    }
+
+
+}
+
